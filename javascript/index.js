ba61888399134b2abc66fdcd90be992f578dceb8
liluehlers <lihuenehlers@gmail.com>
2022-11-07 19:53:31 -0300

    nuevos cambios

---
+++ javascript/index.js
@@ -0,0 +1,13 @@
+let nombre = "Lihuen";
+let apellido = "Ehlers";
+let edad = 35;
+let mascota = "Gato";      
+
+console.log(nombre)
+console.log(apellido)
+console.log(edad)
+console.log(mascota)
+
+let entrada = prompt("Ingrese su nombre");
+let salida = entrada + " " + "Ingresado";
+alert(salida)
